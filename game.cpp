e0f30ca89fa54d67fd58b4411dc3f827a896f5e8
anyazhai <chaithanyachaithanya2708@gmail.com>
2023-05-25 07:08:48 +0530

    bugs fix

--- game.cpp
+++ game.cpp
@@ -1,11 +1,3 @@
-/*******************************************************************
-** This code is part of Breakout.
-**
-** Breakout is free software: you can redistribute it and/or modify
-** it under the terms of the CC BY 4.0 license as published by
-** Creative Commons, either version 4 of the License, or (at your
-** option) any later version.
-******************************************************************/
 #include "game.h"
 #include "resource_manager.h"
 #include "sprite_renderer.h"
@@ -29,8 +21,7 @@ Game::~Game()
 void Game::Init()
 {
     // load shaders
-    ResourceManager::LoadShader("default.vert", "default.frag", nullptr, "sprite");
-
+    ResourceManager::LoadShader("shaders/sprite.vs", "shaders/sprite.frag", nullptr, "sprite");
     // configure shaders
     glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(this->Width),
         static_cast<float>(this->Height), 0.0f, -1.0f, 1.0f);
@@ -38,6 +29,9 @@ void Game::Init()
     ResourceManager::GetShader("sprite").SetMatrix4("projection", projection);
     // set render-specific controls
     Renderer = new SpriteRenderer(ResourceManager::GetShader("sprite"));
+    /*Shader myShader;
+    myShader = ResourceManager::GetShader("sprite");
+    Renderer = new SpriteRenderer(myShader);*/
     // load textures
     ResourceManager::LoadTexture("textures/awesomeface.png", true, "face");
 }
@@ -54,5 +48,8 @@ void Game::ProcessInput(float dt)
 
 void Game::Render()
 {
+    /*Texture2D myTexture;
+    myTexture = ResourceManager::GetTexture("face");
+    Renderer->DrawSprite(myTexture, glm::vec2(200.0f, 200.0f), glm::vec2(300.0f, 400.0f), 45.0f, glm::vec3(0.0f, 1.0f, 0.0f));*/
     Renderer->DrawSprite(ResourceManager::GetTexture("face"), glm::vec2(200.0f, 200.0f), glm::vec2(300.0f, 400.0f), 45.0f, glm::vec3(0.0f, 1.0f, 0.0f));
 }
